8093731ee2ca68fff8a4894d02688a691e1caa70
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2016-04-08 01:50:50 +0200

    Format error messages more nicely and support more error codes :)

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -3,7 +3,8 @@ import random from 'random-item';
 
 const errorMessages = {
   '400': 'you need to provide name for the repo!',
-  '422': 'repo with that name already exists!'
+  '401': 'provided token is invalid!',
+  '422': 'repo with that name already exists or no token was provided!'
 };
 
 const errorIntros = [
@@ -16,5 +17,7 @@ export const errorHandler = error => {
   const errorIntro = chalk.red(random(errorIntros));
   const errorMessage = chalk.red(errorMessages[error.code]);
 
-  console.error(`${errorIntro} ${errorMessage}`);
+  console.log('\n');
+  console.error(`\t${errorIntro} ${errorMessage}`);
+  console.log('\n');
 };
