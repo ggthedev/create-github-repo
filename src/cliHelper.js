942821bf4db549238f6ec77e276ec68741d77919
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2016-04-08 00:43:59 +0200

    Change options in cli to be easier to extract

--- src/cliHelper.js
+++ src/cliHelper.js
@@ -1,16 +1,14 @@
 import commander from 'commander';
 
-commander
+export const cliHelper = commander
   .version('0.0.1')
   .option('-n, --name [name]', 'Repository name', '')
   .option('-d, --desc [desc]', 'Repository description', '')
   .option('-H, --homepage [homepage]', 'Repository homepage', '')
-  .option('-p, --private', 'Should repository be private?', false)
-  .option('-i, --issues', 'Should repository have issues?', true)
-  .option('-w, --wiki', 'Should repository have wiki?', true)
-  .option('-d, --downloads', 'Should repository have downloads?', true)
+  .option('-p, --private', 'Repository should be private', false)
+  .option('-i, --no-issues', 'Repository shouldn\'t have issues', false)
+  .option('-w, --no-wiki', 'Repository shouldn\'t have wiki', false)
+  .option('-d, --no-downloads', 'Repository shouldn\'t have downloads', false)
   .option('-a, --auto-init', 'Should repository have initial commit?', false)
   .option('-g, --gitignore', 'Should repository have wiki?', '')
   .parse(process.argv);
-
-export default commander;
