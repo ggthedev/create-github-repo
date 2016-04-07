f2833862577d11e331724fef895f44361eefa8e2
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2016-04-08 01:31:37 +0200

    Separate functionality to modules

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 import GithubLib from 'github';
 import Promise from 'bluebird';
 
-const repositoryOptions = {
-  name: 'test repo',
-  description: 'test description',
-  homepage: 'test homepage',
-  private: true,
-  has_issues: true,
-  has_wiki: true,
-  has_downloads: true
-};
+import { cliOptions } from './cliHelper';
+import { githubFactory } from './githubFactory';
+import { githubOptionsParser } from './githubOptionsParser';
 
-let github = new GithubLib({
-  version: '3.0.0'
-});
+import { successHandler } from './successHandler';
+import { errorHandler } from './errorHandler';
 
 const githubToken = process.env.TEST_GITHUB_TOKEN;
+const github = githubFactory(githubToken);
 const createRepo = Promise.promisify(github.repos.create);
 
-github.authenticate({
-  type: 'oauth',
-  token: githubToken
-});
-
-createRepo(repositoryOptions)
-  .then(msg => console.log(msg))
-  .catch(err => console.error(err));
+createRepo(githubOptionsParser(cliOptions))
+  .then(success => successHandler(success))
+  .catch(error => errorHandler(error));
